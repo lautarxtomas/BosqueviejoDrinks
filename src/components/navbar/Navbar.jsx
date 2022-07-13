8d3478deb829e9bf74b71252a402a14754613a27
lautarxtomas <lautaro.martinez88@mi.unc.edu.ar>
2022-07-13 20:09:55 -0300

    Agregando menu e-commerce

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+        <button className="navbar_button">Home</button>
+        <button className="navbar_button">Productos</button>
+        <button className="navbar_button">Nosotros</button>
+        <button className="navbar_button">Contacto</button>
+    </div>
+  )
+}
+
+export default navbar
