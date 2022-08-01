fdfccf2936fac2d84eaa997c532002a695bbca20
lautarxtomas <lautaro.martinez88@mi.unc.edu.ar>
2022-07-31 21:40:28 -0300

    agregando Item Detail, solucionando errores

--- src/components/PromoDetail/PromoDetail.jsx
+++ src/components/PromoDetail/PromoDetail.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ItemCount from '../ItemCount'
+// import DetailCard from './DetailCard'
 
 const PromoDetail = ({product, loading}) => {
 
@@ -7,22 +8,29 @@ const PromoDetail = ({product, loading}) => {
 
     <div className="promoDetail">
 
-      {/* MIENTRAS LOADING SE MANTENGA EN TRUE, VA A APARECER EL SPINNER */} 
+      
+    {/* MIENTRAS LOADING SE MANTENGA EN TRUE, VA A APARECER EL SPINNER */} 
       {loading && 
         <div className="d-flex justify-content-center">
           <div className="spinner-border" role="status"></div>
         </div>
       }
 
-      <div className="promoDetailCard">
-          <img src={product.imgSrc} alt="" />
-          <h4>{product.nombre}</h4>
-          <p>${product.precio}</p>
-          <p>{product.desc}</p>
-          <p>{product.descDetail}</p>
-          <ItemCount initial = {product.initial} stock = {product.stock} nombreProducto = {product.nombre} />
-      </div>
+      {loading || // SI NO PONGO ESTO, DURANTE EL LOADING SE PRERENDERIZA LA CARD SIN LOS VALORES CARGADOS, QUEDANDO FEO A LA VISTA.
+        <div className="promoDetailCard">
+            <img src={product.imgSrc} alt="" />
+            <h4>{product.nombre}</h4>
+            <p>${product.precio}</p>
+            <p>{product.desc}</p>
+            <p>{product.descDetail}</p>
+            <ItemCount initial = {product.initial} stock = {product.stock} nombreProducto = {product.nombre} />
+        </div> 
 
+        /* {loading || 
+          <DetailCard nombre={product.nombre} precio={product.precio} desc={product.desc} imgSrc={product.imgSrc} initial={product.initial} stock={product.stock} descDetail={product.descDetail}/>
+        }  */
+
+      }
     </div>
     
   )
