60b50f92eb2a5e936f5fadc57ac9d15fd22728fd
lautarxtomas <lautaro.martinez88@mi.unc.edu.ar>
2022-09-20 13:49:13 -0300

    solucionando deploy

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,11 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <React.StrictMode>
   
     <App />
+
+  </React.StrictMode>
   
 )
